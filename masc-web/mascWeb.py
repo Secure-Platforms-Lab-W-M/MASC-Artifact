d44d68ab729f2f209aa1992569c808da032e062d
scttmars <55801303+scttmars@users.noreply.github.com>
2021-10-12 19:04:13 -0400

    Created mascWeb python file
    Created the python file to work with Flask for the web component

---
+++ masc-web/mascWeb.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app_route('/MASC WEB')
+def MASC_web():
+  return "MASC Home Page"
+
+@app_route('/MASC WEB')
+def MASC_web():
+  return "MASC Home Page"
