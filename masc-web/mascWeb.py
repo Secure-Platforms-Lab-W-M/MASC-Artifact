e66b6e58f0c1feac65995398986c4c0a0ea05102
scttmars <55801303+scttmars@users.noreply.github.com>
2021-10-16 11:44:31 -0700

    Added functionality to help.html
    Added the nav bar and basic functionality to help.html and also rendered the template in python

--- masc-web/mascWeb.py
+++ masc-web/mascWeb.py
@@ -10,6 +10,10 @@ def MASC_web():
 def deploy():
     return render_template("deploy.html")
 
+@app.route('/help/')
+def help():
+    return render_template("help.html")
+
 #@app.route('/Help')
 #def deploy():
 # return "<h1>Help<h1>"
