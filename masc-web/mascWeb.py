c4bb68dcd74e691611b63a5fc89c3b378653dfa7
scttmars <55801303+scttmars@users.noreply.github.com>
2021-10-24 21:18:12 -0400

    Added functionality for python to read file
    This is related to #15

--- masc-web/mascWeb.py
+++ masc-web/mascWeb.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, session
+from os.path import exists
 
 app = Flask(__name__)
 
@@ -14,13 +15,24 @@ def initDeployPage():
 def initHelpPage():
     return render_template("help.html")
 
-@app.route('/lab/')
-def initLabPage():
-    return render_template("lab.html")
+@app.route('/lab/', methods=['GET','POST'])
+def labPage():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save('userFile.java')
+
+    if (exists('userFile.java')):
+        with open('userFile.java', 'r') as userFile:
+            return render_template("lab.html", output=f.userFile)
+    else:
+        return render_template("lab.html")
 
 @app.route('/about/')
 def initAboutPage():
     return render_template("about.html")
+
+
+
 
 #@app.route('/Help')
 #def deploy():
