b5abd1ce91b1bb3311cb74af8cc6033840e8c5b5
TStalnaker44 <28265728+TStalnaker44@users.noreply.github.com>
2021-10-28 16:51:39 -0400

    Added a placeholder name for new operator type

--- masc-core/app/src/main/java/masc/edu/wm/cs/masc/operator/OperatorType.java
+++ masc-core/app/src/main/java/masc/edu/wm/cs/masc/operator/OperatorType.java
@@ -24,4 +24,5 @@ public enum OperatorType {
     AIOSpecificAbstractClassExtendsAbstractClass,
     AIOSpecificAbstractClassImplementsInterface,
     AIOSpecificInterfaceExtendsInterface,
+    TempName,
 }
