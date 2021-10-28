9aab98e4da40f1919e00b53b7fab3ac8529356a0
TStalnaker44 <28265728+TStalnaker44@users.noreply.github.com>
2021-10-28 16:55:20 -0400

    Added a new mutation maker for new operator
    The current name is just a placeholder and will be changed once a suitable name is determined

---
+++ masc-core/app/src/main/java/masc/edu/wm/cs/masc/barebone/mutationmakers/NewMutationMaker.java
@@ -0,0 +1,35 @@
+package masc.edu.wm.cs.masc.barebone.mutationmakers;
+
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+import masc.edu.wm.cs.masc.builders.generic.BuilderMainClass;
+import masc.edu.wm.cs.masc.builders.generic.BuilderMainMethod;
+import masc.edu.wm.cs.masc.operator.OperatorType;
+import masc.edu.wm.cs.masc.operator.restrictive.newoperator.TempName;
+import masc.edu.wm.cs.masc.properties.NewOperatorProperties;
+
+public class NewMutationMaker extends AMutationMaker{
+
+    NewOperatorProperties p;
+
+    public NewMutationMaker(NewOperatorProperties p) {
+        this.p = p;
+    }
+
+    @Override
+    public void populateOperators() {
+        operators.put(OperatorType.TempName, new TempName(p));
+    }
+
+    @Override
+    public String getContent(OperatorType operatorType) {
+        TypeSpec.Builder builder = BuilderMainClass
+                .getClassBody(p.getClassName());
+        System.out.println("Processing: " + operatorType.name());
+        MethodSpec.Builder mainMethod = BuilderMainMethod
+                .getMethodSpecWithException();
+        mainMethod.addCode(operators.get(operatorType).mutation());
+        builder.addMethod(mainMethod.build());
+        return builder.build().toString();
+    }
+}
