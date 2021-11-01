c0efca89a3ff4e4b8bc4e361d758c933e5c4b782
TStalnaker44 <28265728+TStalnaker44@users.noreply.github.com>
2021-11-01 16:12:46 -0400

    Added the ValueInVariable mutation for the IntOperator type

--- masc-core/app/src/main/java/masc/edu/wm/cs/masc/operator/OperatorType.java
+++ masc-core/app/src/main/java/masc/edu/wm/cs/masc/operator/OperatorType.java
@@ -24,5 +24,5 @@ public enum OperatorType {
     AIOSpecificAbstractClassExtendsAbstractClass,
     AIOSpecificAbstractClassImplementsInterface,
     AIOSpecificInterfaceExtendsInterface,
-    TempName,
+    IntValueInVariable,
 }
