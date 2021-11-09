3fe98c7864813a368eaae25ef907ee976182a802
TStalnaker44 <28265728+TStalnaker44@users.noreply.github.com>
2021-11-08 20:33:09 -0500

    Update AIntOperatorTest.java
    Added the abstract keyword to the declaration of AIntOperatorTest

--- masc-core/app/src/test/java/masc/edu.wm.cs.masc/operator/restrictive/intoperator/AIntOperatorTest.java
+++ masc-core/app/src/test/java/masc/edu.wm.cs.masc/operator/restrictive/intoperator/AIntOperatorTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 
-public class AIntOperatorTest {
+public abstract class AIntOperatorTest {
 
     IntOperatorProperties p;
     String expected;
