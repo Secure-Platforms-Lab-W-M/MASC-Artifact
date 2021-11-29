f303b9a9dc277ac59b1315d0de210481325d3f4e
TStalnaker44 <28265728+TStalnaker44@users.noreply.github.com>
2021-11-29 13:51:46 -0500

    Resolved variable naming issues in byte operator
    Issue #60

--- masc-core/app/src/main/java/masc/edu/wm/cs/masc/operator/restrictive/byteoperator/CurrentTime.java
+++ masc-core/app/src/main/java/masc/edu/wm/cs/masc/operator/restrictive/byteoperator/CurrentTime.java
@@ -11,10 +11,10 @@ public class CurrentTime extends AByteOperator {
     @Override
     public String mutation() {
         return "String " + tempVariableName + "=\"\";\n" +
-                "java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat" +
+                "java.text.SimpleDateFormat formatter%d = new java.text.SimpleDateFormat" +
                 "(\"yyyy-MM-dd 'at' HH:mm:ss z\");\n" +
-                "java.sql.Date date = new java.sql.Date(System.currentTimeMillis());\n" +
-                tempVariableName + " = formatter.format(date);\n" +
+                "java.sql.Date date%d = new java.sql.Date(System.currentTimeMillis());\n" +
+                tempVariableName + " = formatter%d.format(date%d);\n" +
                 api_name + " " + api_variable + " = " + "new " + api_name +
                 "(" + tempVariableName + ".getBytes(),0,8);";
     }
