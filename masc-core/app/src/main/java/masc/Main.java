24b1159c1bbdaf11ec78c8076f3ad7565ce0dfbe
TStalnaker44 <28265728+TStalnaker44@users.noreply.github.com>
2021-11-29 13:38:49 -0500

    Removed imports and cleaned code
    Issue #45

--- masc-core/app/src/main/java/masc/Main.java
+++ masc-core/app/src/main/java/masc/Main.java
@@ -2,29 +2,13 @@ package masc;
 
 import masc.edu.wm.cs.masc.barebone.mutationmakers.*;
 import masc.edu.wm.cs.masc.config.PropertiesReader;
-import masc.edu.wm.cs.masc.muse.dataleak.operators.ReachabilityOperator;
-import masc.edu.wm.cs.masc.muse.dataleak.schemas.ReachabilitySchema;
 import masc.edu.wm.cs.masc.muse.dataleak.support.Arguments;
-import masc.edu.wm.cs.masc.muse.dataleak.support.FileUtility;
-import masc.edu.wm.cs.masc.operator.IOperator;
-import masc.edu.wm.cs.masc.operator.OperatorType;
 import masc.edu.wm.cs.masc.operator.RootOperatorType;
 import masc.edu.wm.cs.masc.properties.*;
 import masc.edu.wm.cs.masc.runners.MuseRunner;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.TrueFileFilter;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.Document;
-import org.eclipse.text.edits.MalformedTreeException;
-import org.eclipse.text.edits.TextEdit;
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.Collection;
-import java.util.HashMap;
 
 public class Main {
 
@@ -39,44 +23,48 @@ public class Main {
         else if (!args[0].endsWith(".properties")){
             System.out.println("Properties file must end with the .properties extension");
         }
-//        else if (edu.wm.cs.muse.dataleak.support.Arguments.extractArguments(args[0]) < 0){
-//            System.out.println("Unable to extract arguments from properties file");
-//        }
         else{
-            String path = "new_template.properties";
-            PropertiesReader reader = new PropertiesReader(path);
-            String type = reader.getValueForAKey("type");
-            String scope = reader.getValueForAKey("scope").toUpperCase();
-
-            AMutationMaker m = null;
-            AOperatorProperties p = null;
-
-            if (type.equalsIgnoreCase(RootOperatorType.IntOperator.name())){
-                p = new IntOperatorProperties(path);
-                m = new IntMutationMaker((IntOperatorProperties) p);
-            }
-            else if (type.equalsIgnoreCase(RootOperatorType.StringOperator.name())) {
-                p = new StringOperatorProperties(path);
-                m = new StringOperatorMutationMaker((StringOperatorProperties) p);
-            }
-            else if (type.equalsIgnoreCase(RootOperatorType.ByteOperator.name())) {
-                p = new ByteOperatorProperties(path);
-                m = new ByteMutationMaker((ByteOperatorProperties) p);
-            }
-            else if (type.equalsIgnoreCase(RootOperatorType.Interproc.name())) {
-                p = new InterprocProperties(path);
-                m = new InterprocMutationMaker((InterprocProperties) p);
-            }
-            else if (type.equalsIgnoreCase(RootOperatorType.Flexible.name())) {
-                p = new FlexibleOperatorProperties(path);
-                m = new FlexibleMutationMaker((FlexibleOperatorProperties) p);
-            }
-            else{
-                System.out.println("Unknown Operator Type: " + type);
-                return;
-            }
-            run(scope, reader, m, p);
+            //String path = "new_template.properties";
+            String path = args[0];
+            runMain(path);
         }
+    }
+
+    public static void runMain(String path)
+            throws ConfigurationException, IOException, BadLocationException {
+
+        PropertiesReader reader = new PropertiesReader(path);
+        String type = reader.getValueForAKey("type");
+        String scope = reader.getValueForAKey("scope").toUpperCase();
+
+        AMutationMaker m = null;
+        AOperatorProperties p = null;
+
+        if (type.equalsIgnoreCase(RootOperatorType.IntOperator.name())){
+            p = new IntOperatorProperties(path);
+            m = new IntMutationMaker((IntOperatorProperties) p);
+        }
+        else if (type.equalsIgnoreCase(RootOperatorType.StringOperator.name())) {
+            p = new StringOperatorProperties(path);
+            m = new StringOperatorMutationMaker((StringOperatorProperties) p);
+        }
+        else if (type.equalsIgnoreCase(RootOperatorType.ByteOperator.name())) {
+            p = new ByteOperatorProperties(path);
+            m = new ByteMutationMaker((ByteOperatorProperties) p);
+        }
+        else if (type.equalsIgnoreCase(RootOperatorType.Interproc.name())) {
+            p = new InterprocProperties(path);
+            m = new InterprocMutationMaker((InterprocProperties) p);
+        }
+        else if (type.equalsIgnoreCase(RootOperatorType.Flexible.name())) {
+            p = new FlexibleOperatorProperties(path);
+            m = new FlexibleMutationMaker((FlexibleOperatorProperties) p);
+        }
+        else{
+            System.out.println("Unknown Operator Type: " + type);
+            return;
+        }
+        run(scope, reader, m, p);
     }
 
     public static void run(String scope, PropertiesReader reader,
