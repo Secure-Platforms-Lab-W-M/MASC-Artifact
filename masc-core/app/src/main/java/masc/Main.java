bfa112e0688ac7516e678e4e91226c22dea66503
TStalnaker44 <28265728+TStalnaker44@users.noreply.github.com>
2021-11-29 21:17:24 -0500

    Removed unneeded line of code

--- masc-core/app/src/main/java/masc/Main.java
+++ masc-core/app/src/main/java/masc/Main.java
@@ -24,7 +24,6 @@ public class Main {
             System.out.println("Properties file must end with the .properties extension");
         }
         else{
-            //String path = "newIntOperator.properties";
             String path = args[0];
             runMain(path);
         }
