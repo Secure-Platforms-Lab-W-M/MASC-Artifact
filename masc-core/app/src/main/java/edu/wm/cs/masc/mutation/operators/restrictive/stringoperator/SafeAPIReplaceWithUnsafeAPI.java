22b4c1716bec44b0f891334b66e3279768b7ad41
johnnyclapham <jmclapham@email.wm.edu>
2022-04-30 14:57:42 -0400

    #5 new operator added: SafeAPIReplaceWithUnsafeAPI -> This operator instantiates a the misuse case of using an unsafe API instead of a safe API || EG: HttpURLConnection instead of HttpsURLConnection

---
+++ masc-core/app/src/main/java/edu/wm/cs/masc/mutation/operators/restrictive/stringoperator/SafeAPIReplaceWithUnsafeAPI.java
@@ -0,0 +1,24 @@
+package edu.wm.cs.masc.mutation.operators.restrictive.stringoperator;
+
+import edu.wm.cs.masc.mutation.properties.StringOperatorProperties;
+
+public class SafeAPIReplaceWithUnsafeAPI extends AStringOperator {
+
+    public SafeAPIReplaceWithUnsafeAPI(StringOperatorProperties properties) {
+        super(properties);
+    }
+
+    @Override
+    public String mutation() {
+        StringBuilder s = new StringBuilder();
+        s.append(safe_api_name)
+                .append(".")
+                .append(invocation)
+                .append("(\"").append(parameter).append("\".")
+                .append("replace(\"")
+                .append(safe_api_name).append("\", \"")
+                .append(unsafe_api_name).append("\"));");
+        return s.toString();
+    }
+}
+
