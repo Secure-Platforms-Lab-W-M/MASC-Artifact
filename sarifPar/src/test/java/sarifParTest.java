22866c961ba13ed7a4bed5a7aeacec816eb197c1
scttmars <55801303+scttmars@users.noreply.github.com>
2022-05-12 17:03:24 -0400

    Create sarifParTest.java

---
+++ sarifPar/src/test/java/sarifParTest.java
@@ -0,0 +1,54 @@
+import junit.framework.TestCase;
+
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;
+import org.json.*;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class sarifParTest extends TestCase {
+
+
+    public void testGetResult() {
+        //String sarifPar.getResult();
+    }
+
+    public void testCompareSarifResults() {
+    }
+
+    public void testRemoveLineNumbers() {
+    }
+
+    public void testExtractResult() {
+    }
+///Users/scottmarsden/Documents/reports/source-java-report.sarif /Users/scottmarsden/Documents/reports/class-report.sarif
+    public void testGetJson() throws FileNotFoundException {
+        //JSONObject test = new JSONObject(sarifPar.getJson("source-java-report.sarif"));
+        //if (test.length() < 1 || test.isEmpty()){
+        //    throw exeception;
+       // }
+    }
+
+    public void testCleanList() {
+    }
+
+    public void testMisuseFlowAnalysis() {
+    }
+
+    public void testByteOpFlowAnalysis() {
+    }
+
+    public void testIntOpFlowAnalysis() {
+    }
+
+    public void testInterprocOpFlowAnalysis() {
+    }
+
+    public void testGetJavaMutant() {
+    }
+
+    public void testFindMutation() {
+    }
+}
