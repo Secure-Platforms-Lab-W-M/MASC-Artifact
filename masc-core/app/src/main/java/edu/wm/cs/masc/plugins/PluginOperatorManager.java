84471854ff4bebefd3fad8a9f027b3bfa97a2c87
Yusuf <bsse1013@iit.du.ac.bd>
2022-07-05 16:02:03 +0600

    using default class loader instead of custom

--- masc-core/app/src/main/java/edu/wm/cs/masc/plugins/PluginOperatorManager.java
+++ masc-core/app/src/main/java/edu/wm/cs/masc/plugins/PluginOperatorManager.java
@@ -1,30 +1,28 @@
 package edu.wm.cs.masc.plugins;
 
 import edu.wm.cs.masc.mutation.operators.IOperator;
-import edu.wm.cs.masc.mutation.operators.custom.ACustomGenericOperator;
-import edu.wm.cs.masc.mutation.operators.flexible.AFlexibleOperator;
-import edu.wm.cs.masc.mutation.operators.restrictive.byteoperator.AByteOperator;
-import edu.wm.cs.masc.mutation.operators.restrictive.intoperator.AIntOperator;
-import edu.wm.cs.masc.mutation.operators.restrictive.stringoperator.AStringOperator;
-import edu.wm.cs.masc.mutation.properties.*;
-import org.apache.commons.configuration2.ex.ConfigurationException;
-import org.checkerframework.checker.units.qual.A;
+import edu.wm.cs.masc.mutation.properties.AOperatorProperties;
 
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 
 public class PluginOperatorManager {
     private static final PluginOperatorManager SINGLE_INSTANCE = new PluginOperatorManager();
-    private ArrayList<Class> customOperators = new ArrayList<>();
-    private ArrayList<IOperator> operators = new ArrayList<>();
+    private final ArrayList<Class> customOperators = new ArrayList<>();
+    private final ArrayList<IOperator> operators = new ArrayList<>();
 
     private PluginOperatorManager() {
-        String folderDir = "plugins";
-        folderDir = "app\\build\\libs\\" + folderDir;
-        File[] files = new File(folderDir).listFiles();
+        String packageName = "plugins";
+        String folderDir = "app\\build\\libs";
+//        String folderDir = ".";
+        File[] files = new File(folderDir + "\\" + packageName).listFiles();
+        File folder = new File(folderDir);
 
         if(files == null)
         {
@@ -38,12 +36,11 @@ public class PluginOperatorManager {
                 continue;
 
             try {
-                Class CustomOPClass = loadClass(folderDir, file);
+                Class CustomOPClass = loadClass(folder, packageName, file.getName().split("[.]")[0]);
 
                 if(isOperatorClass(CustomOPClass))
                 {
                     customOperators.add(CustomOPClass);
-                    System.out.println(CustomOPClass);
                 }
             } catch (ClassNotFoundException e) {
                 System.out.println("Class not found: " + file.getName());
@@ -51,11 +48,24 @@ public class PluginOperatorManager {
         }
     }
 
-    private Class loadClass(String folderDir, File file) throws ClassNotFoundException {
-        ClassLoader parentClassLoader = MyClassLoader.class.getClassLoader();
-        MyClassLoader classLoader = new MyClassLoader(parentClassLoader);
-        Class CustomOPClass = classLoader.loadClass(folderDir, file.getName());
-        return CustomOPClass;
+    private Class loadClass(File folder, String packageName, String className) throws ClassNotFoundException {
+
+        try {
+            URL url = folder.toURI().toURL();
+            URL[] urls = new URL[]{url};
+
+            //load this folder into Class loader
+            ClassLoader cl = new URLClassLoader(urls);
+
+            //load the Address class in 'c:\\other_classes\\'
+            Class  cls = cl.loadClass(packageName + "." + className);
+
+            return cls;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
     }
 
     private boolean isOperatorClass(Class customOPClass) {
@@ -73,7 +83,6 @@ public class PluginOperatorManager {
                 AOperatorProperties shit = (AOperatorProperties) c.getParameterTypes()[0].getDeclaredConstructor(String.class).newInstance(path);
                 IOperator operator = (IOperator) customOperator.getDeclaredConstructor(shit.getClass()).newInstance(shit);
                 operators.add(operator);
-                // System.out.println(operator.mutation());
             } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
                 e.printStackTrace();
             }
