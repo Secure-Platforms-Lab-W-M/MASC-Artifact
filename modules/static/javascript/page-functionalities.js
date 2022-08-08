5a4743d1697272933e299f751c4cd8c7ab269f03
RadowanMahmud <bsse1002@iit.du.ac.bd>
2022-08-08 11:16:20 +0600

    login page added

---
+++ modules/static/javascript/page-functionalities.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
