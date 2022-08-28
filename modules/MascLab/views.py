f45006a74297fb9395ad86c4f24136e19b9c22a2
RadowanMahmud <bsse1002@iit.du.ac.bd>
2022-08-28 12:30:11 +0600

    connected to masc main via static path

--- modules/MascLab/views.py
+++ modules/MascLab/views.py
@@ -19,5 +19,9 @@ async def run(cmd):
         print(f'[stderr]\n{stderr.decode()}')
 
 def index(request):
+    # operator
+    # make any.properties file 
     s = asyncio.run(run('node --version'))
+    p = asyncio.run(run('java D:\8th\spl\masc\MASC-SFall2022\masc-core\\app\src\main\java\edu\wm\cs\masc\MASC.java cipher.properties'))
+    # read the output file
     return render(request, "masc-lab/lab.html")
