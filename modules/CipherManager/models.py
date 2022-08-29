debaf94aa72d677c575e5d1ae21fd3e9235dd875
RadowanMahmud <bsse1002@iit.du.ac.bd>
2022-08-30 02:20:43 +0600

    uploading propert5ies file to server

--- modules/CipherManager/models.py
+++ modules/CipherManager/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class PropertiesList(models.Model):
+    name = models.CharField(max_length=200)
+    path = models.CharField(max_length=500)
+    type = models.CharField(max_length=50)
+    file = models.FileField()
+
 # Create your models here.
--- modules/CipherManager/views.py
+++ modules/CipherManager/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 def index(request):
     uploaded_files_header = ["Id", "File Name", "Path", "Type"]
@@ -5,10 +6,18 @@ def index(request):
         "uploaded_files_header": uploaded_files_header
     })
 
+def handle_uploaded_file(f):
+    with open('./modules/static/properties/'+f.name, 'wb') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
 def uploadPropertyForm(request):
     if request.method == 'POST':
         print('ok')
-        print(request.body)
+        print(request.POST['name'])
+        print(request.POST['type'])
+        print(request.FILES)
+        handle_uploaded_file(request.FILES['file'])
         return render(request,'CipherManager/thanks.html')
     return render(request, "CipherManager/uploadProperty.html")
 # Create your views here.
