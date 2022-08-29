a7c82b3461d8728c704478bc8af2d85586fc531a
RadowanMahmud <bsse1002@iit.du.ac.bd>
2022-08-30 03:45:36 +0600

    user can now upload properties file

--- modules/CipherManager/models.py
+++ modules/CipherManager/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class PropertiesList(models.Model):
     name = models.CharField(max_length=200)
+    filename = models.CharField(max_length=100, db_index=True, default="any.properties")
     path = models.CharField(max_length=500)
     type = models.CharField(max_length=50)
-    file = models.FileField()
 
 # Create your models here.
--- modules/CipherManager/views.py
+++ modules/CipherManager/views.py
@@ -1,7 +1,12 @@
 import json
 from django.shortcuts import render
+from modules.CipherManager.models import PropertiesList
+
+
 def index(request):
     uploaded_files_header = ["Id", "File Name", "Path", "Type"]
+    records = PropertiesList.objects.all().values()
+    print(records)
     return render(request, "CipherManager/index.html", {
         "uploaded_files_header": uploaded_files_header
     })
@@ -10,14 +15,16 @@ def handle_uploaded_file(f):
     with open('./modules/static/properties/'+f.name, 'wb') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+    return destination.name
 
 def uploadPropertyForm(request):
     if request.method == 'POST':
-        print('ok')
-        print(request.POST['name'])
-        print(request.POST['type'])
-        print(request.FILES)
-        handle_uploaded_file(request.FILES['file'])
+        name = request.POST['name']
+        ptype = request.POST['type']
+        filename = request.FILES['file'].name
+        path = handle_uploaded_file(request.FILES['file']) # path from masc core shall be added
+        data = PropertiesList(name = name,type = ptype, filename = filename, path = path);
+        data.save()
         return render(request,'CipherManager/thanks.html')
     return render(request, "CipherManager/uploadProperty.html")
 # Create your views here.
