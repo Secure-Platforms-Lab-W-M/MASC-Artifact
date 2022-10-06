7b76b30a036b246f0099cec52c13797b66b98ea8
RadowanMahmud <bsse1002@iit.du.ac.bd>
2022-10-06 12:19:55 +0600

    source code now upload able

--- modules/MascEngine/models.py
+++ modules/MascEngine/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class MASCEngineProcesses(models.Model):
+    id = models.CharField(max_length=200, primary_key=True)
+    zip_file_name = models.CharField(max_length=100, db_index=True)
+    properties = models.CharField(max_length=500)
+    input_path = models.CharField(max_length=500)
+    output_path = models.CharField(max_length=500)
+    appName = models.CharField(max_length=50)
+    scope = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
--- modules/MascEngine/views.py
+++ modules/MascEngine/views.py
@@ -17,9 +17,20 @@ def read_selected_file(f):
     return content
 
 
+def handle_uploaded_file(f):
+    with open('./modules/static/sourcecodes/'+f.name, 'wb') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    return destination.name
+
+
 def runMASCEngine(request):
     if request.method == 'POST':
-        scope = request.POST['scope']
+        scopes = request.POST['scope']
+        properties_file = request.POST['file_name']
+        contents = request.POST['content']
+        input_path = handle_uploaded_file(request.FILES['sourcecode']) # path from masc core shall be added
+        print(input_path)
     custome_operator_headers = ["Uploaded File", "Selected Operator", "Status", "Actions"]
     return render(request, "masc-engine/history.html", {
         "custome_operator_headers": custome_operator_headers
